aca10362bfba93c0573caf700121c878fd1028ea
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-12 04:20:18 +0100

    chore: update dev.js file

--- config/dev.js
+++ config/dev.js
@@ -3,6 +3,6 @@ module.exports = {
     "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
   googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
   mongoURI:
-    "mongodb+srv://balogunmuhammadawwal:<password>@blogger.tg6iqz9.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://balogunmuhammadawwal:Mwtm6G21i22c3k8y@blogger.tg6iqz9.mongodb.net/?retryWrites=true&w=majority",
   cookieKey: "123123123",
 };
