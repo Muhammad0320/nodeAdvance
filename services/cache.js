04e0beb3dfd2842b6ad73b6a847518b453dcf006
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-14 18:09:13 +0100

    chore: add prettier config

--- services/cache.js
+++ services/cache.js
@@ -3,5 +3,7 @@ const mongoose = require("mongoose");
 const { exec } = mongoose.Query.prototype;
 
 mongoose.Query.prototype.exec = function () {
+  console.log("I ' M ABOUT TO RUN A QUERY");
+
   return exec.apply(this, arguments);
 };
