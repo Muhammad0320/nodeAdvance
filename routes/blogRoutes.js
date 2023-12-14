118407c4bb9a20e2b4ba19743c612286d6f2c226
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-14 18:15:35 +0100

    chore: add blogroutes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,14 +1,14 @@
-const redis = require("redis");
-const mongoose = require("mongoose");
+const redis = require('redis');
+const mongoose = require('mongoose');
 
-const util = require("util");
+const util = require('util');
 
-const requireLogin = require("../middlewares/requireLogin");
+const requireLogin = require('../middlewares/requireLogin');
 
-const Blog = mongoose.model("Blog");
+const Blog = mongoose.model('Blog');
 
-module.exports = (app) => {
-  app.get("/api/blogs/:id", requireLogin, async (req, res) => {
+module.exports = app => {
+  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
       _id: req.params.id,
@@ -17,33 +17,13 @@ module.exports = (app) => {
     res.send(blog);
   });
 
-  app.get("/api/blogs", requireLogin, async (req, res) => {
+  app.get('/api/blogs', requireLogin, async (req, res) => {
     const blogs = await Blog.find({ _user: req.user.id });
 
-    const redisUrl = "redis://127.0.0.1:6379";
-
-    const client = redis.createClient(redisUrl);
-
-    client.get = util.promisify(client.get);
-
-    const cachedBlog = await client.get(req.user.id);
-
-    console.log("What is happening");
-
-    if (cachedBlog) {
-      console.log("From redis");
-
-      return res.send(JSON.parse(cachedBlog));
-    }
-
-    console.log("From mongoDB");
-
     res.send(blogs);
-
-    client.set(req.user.id, JSON.stringify(blogs));
   });
 
-  app.post("/api/blogs", requireLogin, async (req, res) => {
+  app.post('/api/blogs', requireLogin, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
