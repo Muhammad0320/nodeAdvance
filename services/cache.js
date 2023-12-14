0fa7fa9056764d11d34f894600535fa562094f97
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-14 18:12:09 +0100

    chore: update cache

--- services/cache.js
+++ services/cache.js
@@ -1,9 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const { exec } = mongoose.Query.prototype;
 
 mongoose.Query.prototype.exec = function () {
-  console.log("I ' M ABOUT TO RUN A QUERY");
+  console.log("I'M ABOUT TO RUN A QUERY");
+  //   console.log('Let\'s see ');
 
   return exec.apply(this, arguments);
 };
