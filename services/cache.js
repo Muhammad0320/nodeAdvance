d374f0523d101bc576da0d3eee05957db97eda91
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-15 16:02:18 +0100

    chore: update chaching logic

--- services/cache.js
+++ services/cache.js
@@ -26,11 +26,11 @@ mongoose.Query.prototype.exec = async function () {
   const cachedValue = await client.get(key);
 
   if (cachedValue) {
-    console.log(cachedValue);
+    const doc = JSON.parse(cachedValue);
 
-    const doc = new this.model(JSON.parse(cachedValue));
-
-    return doc;
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc);
   }
 
   const result = await exec.apply(this, arguments);
@@ -45,33 +45,7 @@ mongoose.Query.prototype.exec = async function () {
 /*
 
 
-const redis = require('redis');
 
-const util = require('util');
-
-             const blogs = await Blog.find({ _user: req.user.id });
-
-    const redisUrl = "redis://127.0.0.1:6379";
-
-    const client = redis.createClient(redisUrl);
-
-    client.get = util.promisify(client.get);
-
-    const cachedBlog = await client.get(req.user.id);
-
-    console.log("What is happening");
-
-    if (cachedBlog) {
-      console.log("From redis");
-
-      return res.send(JSON.parse(cachedBlog));
-    }
-
-    console.log("From mongoDB");
-
-    res.send(blogs);
-
-    client.set(req.user.id, JSON.stringify(blogs));
 
 
 */
