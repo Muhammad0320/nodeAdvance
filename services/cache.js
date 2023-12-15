d48961318becc1f5023a57fa2428c3bbb1bcfe0c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-15 17:14:16 +0100

    chore: update cache comp

--- services/cache.js
+++ services/cache.js
@@ -6,13 +6,11 @@ const redisUrl = 'redis://127.0.0.1:6379';
 
 const client = redis.createClient(redisUrl);
 
-// client.get = util.promisify(client.get);
+client.get = util.promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = async function () {
-  client.get = util.promisify(client.get);
-
   //   console.log({ ...this.getQuery(), collection: this.mongooseCollection.name });
 
   const key = JSON.stringify(
@@ -23,14 +21,13 @@ mongoose.Query.prototype.exec = async function () {
 
   const cachedValue = await client.get(key);
 
+  console.log(cachedValue);
+  console.log(key);
+
   if (cachedValue) {
-    console.log('odehhhhhhhhhhhhhhhhhh');
-
-    console.log(cachedValue);
-
     const doc = JSON.parse(cachedValue);
-    console.log('hjdbehjvbfehv elh');
-    console.log(doc);
+    // console.log('hjdbehjvbfehv elh');
+    // console.log(doc);
 
     return Array.isArray(doc)
       ? doc.map(d => new this.model(d))
