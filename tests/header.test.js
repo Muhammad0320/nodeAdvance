0ca5b7c98d8088045bb452a95ca9a954365141f6
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-16 11:24:57 +0100

    chore: add test to launch browser

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,5 +1,13 @@
+const puppeteer = require('puppeteer');
+
 test('Add two numbers', () => {
   const sum = 5 + 5;
 
   expect(sum).toEqual(10);
+});
+
+test('we can launch the browser', async () => {
+  const browser = await puppeteer.launch({ headless: false });
+
+  const page = await browser.newPage();
 });
