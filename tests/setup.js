fba9c7dea6e8e78c9a4fcea7af676ba5bb939a69
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-18 19:20:19 +0100

    chore: add setup

--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,12 @@
 require('../models/User');
 
+const keys = require('../config/keys');
+
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
