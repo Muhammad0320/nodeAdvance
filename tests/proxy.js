dfd13a92a37e430d1253c42c9d0987e5059e0073
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-19 18:26:15 +0100

    chore: check proxy

---
+++ tests/proxy.js
@@ -0,0 +1,19 @@
+class Greetings {
+  constructor() {}
+
+  english() {
+    return 'hello';
+  }
+
+  spanish() {
+    return 'hallo';
+  }
+}
+
+class MoreGreetings {
+  constructor() {}
+
+  german() {
+    return 'hallo';
+  }
+}
