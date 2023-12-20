f21debbcc8674a1c84f9dbb4595e2b28fc3244e2
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-20 05:49:19 +0100

    chore: update header test

--- tests/header.test.js
+++ tests/header.test.js
@@ -30,17 +30,7 @@ test('clicking login starts oauth flow', async () => {
 });
 
 test('show logout button, when signed in', async () => {
-  const user = await userFactory();
-
-  const { session, sig } = sessionFactory(user);
-
-  await page.setCookie({ name: 'session', value: session });
-
-  await page.setCookie({ name: 'session.sig', value: sig });
-
-  await page.goto('localhost:3000');
-
-  await page.waitFor('a[href="/auth/logout"]');
+  await page.login();
 
   const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
 
